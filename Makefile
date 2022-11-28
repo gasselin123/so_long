720150076ef8b03fe2454e7e1eff0c5108fe2982
gasselin42 <gabrielasselin@MacBook-Air-de-Gabriel.local>
2022-11-28 09:20:00 -0500

    Revamped the whole project

--- Makefile
+++ Makefile
@@ -6,50 +6,108 @@
 #    By: gasselin <gasselin@student.42quebec.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/22 11:49:55 by gasselin          #+#    #+#              #
-#    Updated: 2022/01/19 15:28:13 by gasselin         ###   ########.fr        #
+#    Updated: 2022/11/28 09:03:16 by gasselin         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-NAME =		so_long
-OBJS_PATH =	objs
+NAME		=	so_long
 
-SRCS =		so_long.c move_enemy.c ft_is_map_valid.c ft_manage_timer.c \
-			ft_move.c ft_print_map.c ft_utils.c ft_utils2.c
+SRC			=	srcs
+INC			=	includes
+OBJ			=	objs
 
-CC =		gcc
-CFLAGS =	-Wall -Werror -Wextra -c -g
-INCLUDES =	-Imlx -I.
-LIBS =		-lmlx -framework OpenGL -framework AppKit
-OBJS =		$(SRCS:.c=.o)
+LIB			=	lib
+LIBFT		=	$(LIB)/libft
+MLX_LINUX	=	$(LIB)/mlx_linux
+MLX_MAC 	= 	$(LIB)/mlx_mac
 
-SRCS_FULL =	$(addprefix srcs/, $(SRCS))
-OBJS_FULL =	$(addprefix objs/, $(OBJS))
+CC			=	gcc
+CFLAGS		=	-Wall -Werror -Wextra -g
 
-all: $(OBJS_PATH) $(NAME)
+YELLOW 	= \033[0;33m
+GREEN 	= \033[0;32m
+RED 	= \033[0;31m
+END 	= \033[0m
 
-$(OBJS_PATH):
-	@mkdir -p $(OBJS_PATH)
-	@echo Created: Object directory
+RM			=	rm -rf
+MK			=	mkdir -p
+NM			=	norminette
 
-$(NAME): $(OBJS_FULL)
-	@make re --no-print-directory -C ./ft_printf
-	@$(CC) $(OBJS_FULL) $(LIBS) -o $(NAME) ./ft_printf/libftprintf.a
-	@echo "\\n\033[32;1m SO_LONG HAS BEEN GENERATED \033[0m \\n"
+MAKE_DIR	= $(MAKE) -C
 
-$(OBJS_PATH)/%.o: ./%.c
-	@echo "Created: $@\033[1A\033[M"
-	@$(CC) $(CFLAGS) $(INCLUDES) $< -o $@
+CFILES		=	so_long.c \
+						ft_is_map_valid.c \
+						ft_manage_timer.c \
+						ft_move.c \
+						ft_print_map.c \
+						ft_utils.c \
+						ft_utils2.c \
+						move_enemy.c \
 
+HFILES		=	so_long.h
+				
+OFILES		 =	$(CFILES:.c=.o)
+
+SRCS			=	$(addprefix $(SRC)/, $(CFILES))
+HEADERS			=	$(addprefix $(INC)/, $(HFILES))
+OBJS			=	$(addprefix $(OBJ)/, $(OFILES))
+
+VPATH		=	$(SRCS)
+
+FRAMEWORKS_MAC 		= -lft -lmlx -lm -framework OpenGL -framework AppKit
+FRAMEWORKS_LINUX 	= -lft -lmlx -lm -lXext -lbsd -lX11
+
+$(OBJ)/%.o:	$(SRC)/%.c
+			$(CC) $(CFLAGS) -I$(INC) -I$(LIB) -c $< -o $@
+
+all:		$(NAME)
+
+$(NAME):	$(OBJ) $(OBJS)
+		@echo "$(GREEN) Compiling..."
+		@$(MAKE_DIR) $(LIBFT)
+		@$(MAKE_DIR) $(MLX_MAC)
+		$(CC) $(OBJS) -L$(LIBFT) -L$(MLX_MAC) $(FRAMEWORKS_MAC) -o $(NAME)
+		@echo "$(END)"
+		@echo "$(GREEN)  So Long Compiled\n $(END)"
+
+$(OBJ):
+			@$(MK) $(OBJ)
+
+linux:		$(OBJ) $(OBJS)
+			@echo "$(GREEN) Compiling..."
+			@$(MAKE_DIR) $(LIBFT)
+			@$(MAKE_DIR) $(MLX_LINUX)
+			$(CC) $(OBJS)  -L$(LIBFT) -L$(MLX_LINUX) $(FRAMEWORKS_LINUX) -o $(NAME)
+			@echo "$(GREEN) Cub3D Compiled\n $(END)"
+			
 clean:
-	@make clean --no-print-directory -C ./ft_printf
-	@rm -rf $(OBJS_FULL) $(OBJS_PATH) 
-	@echo "\033[34;1m CLEANED OBJECT FILES \033[0m"
+			@$(MAKE_DIR) $(LIBFT) clean
+			@$(MAKE_DIR) $(MLX_MAC) clean
+			@$(RM) $(OBJS)
+			@$(RM) $(OBJ)
 
-fclean: clean
-	@make fclean --no-print-directory -C ./ft_printf
-	@rm -f $(NAME)
-	@echo "\033[34;1m CLEANED SO_LONG \033[0m"
+linux-clean:
+			@echo "$(RED) Cleaning..."
+			@$(MAKE_DIR) $(LIBFT) clean
+			@$(MAKE_DIR) $(MLX_LINUX) clean
+			@$(RM) $(OBJS)
+			@$(RM) $(OBJ)
+			@echo "$(END)"
 
-re: fclean all
+fclean:		clean
+			@echo "$(RED) Removing executable..."
+			@$(MAKE_DIR) $(LIBFT) fclean
+			@$(RM) $(NAME)
+			@echo "$(END)"
 
-.PHONY: all clean fclean re
+re:			fclean all
+
+relinux:			fclean linux
+
+norm:
+			@$(NM) $(SRCS) $(HEADERS)
+
+val: all
+	valgrind   --leak-check=full --track-origins=yes -s --trace-children=yes  ./$(NAME) map.cub
+
+.PHONY:		all debug clean fclean re linux
---
+++ srcs/ft_print_map.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_map.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gasselin <gasselin@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/30 10:28:50 by gasselin          #+#    #+#             */
+/*   Updated: 2022/11/28 09:08:24 by gasselin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	ft_parse_tiles2(t_vars *vars, int i, int j)
+{
+	if (vars->maps[i][j] != '1')
+		mlx_put_image_to_window(vars->mlx, vars->win,
+			vars->floor[vars->animation_floor / 10].img, j * 32, i * 32);
+	if (vars->maps[i][j] == '1')
+		mlx_put_image_to_window(vars->mlx, vars->win, vars->img.wall,
+			j * 32, i * 32);
+	if (vars->maps[i][j] == 'C')
+		mlx_put_image_to_window(vars->mlx, vars->win,
+			vars->coin[vars->animation_coin / 10].img, j * 32, i * 32);
+	if (vars->maps[i][j] == 'P' || vars->maps[i][j] == 'D')
+		mlx_put_image_to_window(vars->mlx, vars->win,
+			vars->plrs[vars->animation_enp / 30].img, j * 32, i * 32);
+	if (vars->maps[i][j] == 'U')
+		mlx_put_image_to_window(vars->mlx, vars->win,
+			vars->plrw[vars->animation_enp / 30].img, j * 32, i * 32);
+	if (vars->maps[i][j] == 'L')
+		mlx_put_image_to_window(vars->mlx, vars->win,
+			vars->plra[vars->animation_enp / 30].img, j * 32, i * 32);
+	if (vars->maps[i][j] == 'R')
+		mlx_put_image_to_window(vars->mlx, vars->win,
+			vars->plrd[vars->animation_enp / 30].img, j * 32, i * 32);
+	if (vars->maps[i][j] == 'E')
+		mlx_put_image_to_window(vars->mlx, vars->win,
+			vars->exit[vars->animation_exit / 30].img, j * 32, i * 32);
+	print_ghosts(vars, i, j);
+}
+
+int	ft_parse_tiles(t_vars *vars)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	find_spawn(vars);
+	manage_timer(vars);
+	while (i < vars->line_count)
+	{
+		j = 0;
+		while (j < vars->width)
+		{
+			ft_parse_tiles2(vars, i, j);
+			j++;
+		}
+		i++;
+	}
+	print_nbmoves(vars);
+	return (0);
+}
+
+void	ft_set_img2(t_vars *vars)
+{
+	vars->plra[0].img = read_tileset(vars, "./tileset/left.xpm");
+	vars->plra[1].img = read_tileset(vars, "./tileset/left1.xpm");
+	vars->plra[2].img = read_tileset(vars, "./tileset/left2.xpm");
+	vars->plrs[0].img = read_tileset(vars, "./tileset/down.xpm");
+	vars->plrs[1].img = read_tileset(vars, "./tileset/down1.xpm");
+	vars->plrs[2].img = read_tileset(vars, "./tileset/down2.xpm");
+	vars->plrd[0].img = read_tileset(vars, "./tileset/right.xpm");
+	vars->plrd[1].img = read_tileset(vars, "./tileset/right1.xpm");
+	vars->plrd[2].img = read_tileset(vars, "./tileset/right2.xpm");
+	vars->ghost1[0].img = read_tileset(vars, "./tileset/ghost1.xpm");
+	vars->ghost1[1].img = read_tileset(vars, "./tileset/ghost1-1.xpm");
+	vars->ghost1[2].img = read_tileset(vars, "./tileset/ghost1-2.xpm");
+	vars->ghost2[0].img = read_tileset(vars, "./tileset/ghost2.xpm");
+	vars->ghost2[1].img = read_tileset(vars, "./tileset/ghost2-1.xpm");
+	vars->ghost2[2].img = read_tileset(vars, "./tileset/ghost2-2.xpm");
+	vars->ghost3[0].img = read_tileset(vars, "./tileset/ghost3.xpm");
+	vars->ghost3[1].img = read_tileset(vars, "./tileset/ghost3-1.xpm");
+	vars->ghost3[2].img = read_tileset(vars, "./tileset/ghost3-2.xpm");
+	vars->ghost4[0].img = read_tileset(vars, "./tileset/ghost4.xpm");
+	vars->ghost4[1].img = read_tileset(vars, "./tileset/ghost4-1.xpm");
+	vars->ghost4[2].img = read_tileset(vars, "./tileset/ghost4-2.xpm");
+}
+
+void	ft_set_img(t_vars *vars)
+{
+	vars->img.wall = read_tileset(vars, "./tileset/wall.xpm");
+	vars->exit[0].img = read_tileset(vars, "./tileset/exit.xpm");
+	vars->exit[1].img = read_tileset(vars, "./tileset/exit1.xpm");
+	vars->exit[2].img = read_tileset(vars, "./tileset/exit2.xpm");
+	vars->coin[0].img = read_tileset(vars, "./tileset/coin1.xpm");
+	vars->coin[1].img = read_tileset(vars, "./tileset/coin2.xpm");
+	vars->coin[2].img = read_tileset(vars, "./tileset/coin3.xpm");
+	vars->coin[3].img = read_tileset(vars, "./tileset/coin4.xpm");
+	vars->coin[4].img = read_tileset(vars, "./tileset/coin5.xpm");
+	vars->coin[5].img = read_tileset(vars, "./tileset/coin6.xpm");
+	vars->floor[0].img = read_tileset(vars, "./tileset/floor.xpm");
+	vars->floor[1].img = read_tileset(vars, "./tileset/floor1.xpm");
+	vars->floor[2].img = read_tileset(vars, "./tileset/floor2.xpm");
+	vars->floor[3].img = read_tileset(vars, "./tileset/floor3.xpm");
+	vars->floor[4].img = read_tileset(vars, "./tileset/floor4.xpm");
+	vars->floor[5].img = read_tileset(vars, "./tileset/floor5.xpm");
+	vars->floor[6].img = read_tileset(vars, "./tileset/floor6.xpm");
+	vars->floor[7].img = read_tileset(vars, "./tileset/floor7.xpm");
+	vars->plrw[0].img = read_tileset(vars, "./tileset/up.xpm");
+	vars->plrw[1].img = read_tileset(vars, "./tileset/up1.xpm");
+	vars->plrw[2].img = read_tileset(vars, "./tileset/up2.xpm");
+	ft_set_img2(vars);
+}
