017cb029f760619fb279140283b755fd46800999
gasselin42 <gabrielasselin@MacBook-Air-de-Gabriel.local>
2022-11-28 08:39:21 -0500

    Deleted unused variable

--- ft_printf/libft/ft_count_char.c
+++ ft_printf/libft/ft_count_char.c
@@ -6,7 +6,7 @@
 /*   By: gasselin <gasselin@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 09:38:09 by gasselin          #+#    #+#             */
-/*   Updated: 2021/07/12 11:13:03 by gasselin         ###   ########.fr       */
+/*   Updated: 2022/11/28 08:37:50 by gasselin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ int	ft_count_char(const char *s, const char *set)
 	int		count;
 	int		i;
 	int		j;
-	size_t	length;
 
 	count = 0;
 	i = 0;
 	if (!s || !set)
 		return (0);
-	length = ft_strlen(set);
 	while (s[i])
 	{
 		j = 0;
